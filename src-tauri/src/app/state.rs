a96bc48ba8c6f43893cb89631db48a12ffbf8299
nasu <168271934+Nasu726@users.noreply.github.com>
2026-09-13 17:16:08 +0900

    feat: store current view state in Rust app state

--- src-tauri/src/app/state.rs
+++ src-tauri/src/app/state.rs
@@ -1,14 +1,17 @@
+use super::ViewState;
 use rusqlite::Connection;
 use std::sync::Mutex;
 
 pub(crate) struct AppState {
     pub(crate) db: Mutex<Connection>,
+    pub(crate) view: Mutex<ViewState>,
 }
 
 impl AppState {
     pub(crate) fn new(connection: Connection) -> Self {
         Self {
             db: Mutex::new(connection),
+            view: Mutex::new(ViewState::default()),
         }
     }
 }

7dcd3949c77ca534a096a44f67cc74107558f6db
nasu <168271934+Nasu726@users.noreply.github.com>
2026-09-13 17:19:07 +0900

    feat: expose Rust view controller

--- src-tauri/src/app/mod.rs
+++ src-tauri/src/app/mod.rs
@@ -1,5 +1,7 @@
 mod state;
+mod view_controller;
 mod view_state;
 
 pub(crate) use state::AppState;
+pub(crate) use view_controller::{apply_current as apply_current_view, current as current_view, transition as transition_view};
 pub(crate) use view_state::{ViewEvent, ViewState};

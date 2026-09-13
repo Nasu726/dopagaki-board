5451164cc0f26b012c92a8d7f011658382b2292b
nasu <168271934+Nasu726@users.noreply.github.com>
2026-09-13 17:28:05 +0900

    style: apply rustfmt to app exports

--- src-tauri/src/app/mod.rs
+++ src-tauri/src/app/mod.rs
@@ -3,5 +3,7 @@ mod view_controller;
 mod view_state;
 
 pub(crate) use state::AppState;
-pub(crate) use view_controller::{apply_current as apply_current_view, current as current_view, transition as transition_view};
+pub(crate) use view_controller::{
+    apply_current as apply_current_view, current as current_view, transition as transition_view,
+};
 pub(crate) use view_state::{ViewEvent, ViewState};
